b595747c032c84825a23af78f343243d360a65c5
matheotestadi <115212577+matheotestadi@users.noreply.github.com>
2022-10-07 10:02:25 +0200

    Add files via upload

---
+++ cercle.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+#include <cmath>
+#include "cercle.h"
+
+int main(){
+    return 0;
+}
+
+Cercle :: Cercle(float ray){
+    this -> rayon = ray;
+}
+Cercle:: ~Cercle(){
+}
+
+float Cercle::perimetre(){
+    int perimetre = 2*pi*c.rayon
+            return perimetre;
+}
+
+float Cercle::surface(){
+    int surface = pi*(c.rayon*c.rayon)
+    return surface;
+}
---
+++ cercle.h
@@ -0,0 +1,17 @@
+/* Created by matheo on 07/10/2022 */
+#include <Cmath>
+#ifndef UNTITLED2_CERCLE_H
+#define UNTITLED2_CERCLE_H
+
+#endif //UNTITLED2_CERCLE_H
+
+class Cercle {
+public:
+    Cercle(float ray);
+    ~Cercle();
+    float perimetre();
+    float surface();
+
+public:
+    float rayon;
+};
